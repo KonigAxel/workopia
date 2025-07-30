422bd7b020c7514a310071a34a8e16d9416b5250
KonigAxel <konig.axel94@gmail.com>
2025-07-30 21:24:13 +0200

    a

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Listings</title>
+</head>
+<body>
+    <h1>{{$title}}</h1>
+    <!-- @if(!empty($jobs)) -->
+        <p>Available jobs:</p>
+    <!-- <ul>
+        @foreach($jobs as $job)
+        <li>
+            {{ $job }}
+        </li>
+        @endforeach
+    </ul>
+    @else
+        <p>No jobs available at the moment.</p>
+    @endif -->
+
+
+
+    <ul>
+        @forelse($jobs as $job)
+        <li>{{$loop->index}} - {{ $job }}</li>
+        @empty
+            <p>No jobs available at the moment.</p>
+        @endforelse
+    </ul>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+
+    $title = 'Available Jobs';
+    $jobs = [
+        'Web Dev',
+        'PHP developer',
+        'Db Admin',
+        'Software Engineer',
+        'Sys Admin'
+    ];
+
+    
+    return view('jobs.index', compact('title','jobs'));
+})->name('jobs');
+
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
+
+
+// Route::get('/test', function () {   
+//     return response()->json(['name' => 'John Doe'])->cookie('name', 'Brad');
+// });
+
+// Route::get('/download', function () {
+//     return response()->download(public_path('favicon.ico'));
+// });
