f92b7849e593361a15fb604be69a893096658671
KonigAxel <konig.axel94@gmail.com>
2025-08-03 19:19:14 +0200

    0509

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    @vite('resources/css/app.css')
     <title>{{$title ?? 'Workopia jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header />
-    <h1>Layout component</h1>
+    
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
