1427d40d575628ef23c8a87612e5c73512dbf054
KonigAxel <konig.axel94@gmail.com>
2025-08-10 18:07:51 +0200

    11

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,14 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <title>{{$title ?? 'Workopia jobs'}}</title>
 </head>
 <body class="bg-gray-100">
     <x-header />
-    
+    @if(request()->is('/'))
+        <x-hero />
+    @endif
     <main class="container mx-auto p-4 mt-4">
         {{$slot}}
     </main>
     
+    <script src="{{asset('js/script.js')}}"></script>
 </body>
 </html> 
